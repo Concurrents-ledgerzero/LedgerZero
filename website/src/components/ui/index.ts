a4bb36e6319631df7432048b52b785093bb58df5
MILAN BHADARKA <bhadarkamilan3@gmail.com>
2026-01-29 10:39:11 +0530

    Build

--- website/src/components/ui/index.ts
+++ website/src/components/ui/index.ts
@@ -1,7 +1,7 @@
-export { Button } from './Button.tsx';
-export { Input, OtpInput, MpinInput } from './Input.tsx';
-export { Card, StatCard, TransactionCard, BankCard, QuickAction } from './Card.tsx';
-export { Modal, BottomSheet, ConfirmDialog } from './Modal.tsx';
-export { Spinner, Skeleton, PageLoader, ProcessingLoader, PulseDots } from './Loader.tsx';
-export { NetworkGraph } from './NetworkGraph.tsx';
-export type { TransactionGraphData, GraphNode, GraphLink, RawTransaction } from './NetworkGraph.tsx';
+export { Button } from './Button';
+export { Input, OtpInput, MpinInput } from './Input';
+export { Card, StatCard, TransactionCard, BankCard, QuickAction } from './Card';
+export { Modal, BottomSheet, ConfirmDialog } from './Modal';
+export { Spinner, Skeleton, PageLoader, ProcessingLoader, PulseDots } from './Loader';
+export { NetworkGraph } from './NetworkGraph';
+export type { TransactionGraphData, GraphNode, GraphLink, RawTransaction } from './NetworkGraph';
